830f5c0aefe1fea7bbe7122aa9637e74da9f2023
ThomasLacroix1 <144111645+ThomasLacroix1@users.noreply.github.com>
2024-09-27 14:32:32 +0200

    t-lac | data change

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
